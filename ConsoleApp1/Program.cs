94780d7ce74683540417de65ccf3b0ee674fa7d8
user <user@PC116-11>
2023-06-02 12:50:52 +0500

    Message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!"); //Всем программистам, шалом!
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32( Console.ReadLine());
+            Random rnd = new Random();            
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine(rnd.Next(1, 99) + rnd.Next(1, 99));
+                    break;
+                case 2:
+                    Console.WriteLine(rnd.Next(1, 99) - rnd.Next(1, 99));
+                    break;
+                case 3:
+                    Console.WriteLine(rnd.Next(1, 99) * rnd.Next(1, 99));
+                    break;
+                case 4:
+                    Console.WriteLine(rnd.Next(1, 99) / rnd.Next(1, 99));
+                    break;
+               default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+
+            //Задержка экрана
+            Console.ReadKey();
+        }
+    }
+}
